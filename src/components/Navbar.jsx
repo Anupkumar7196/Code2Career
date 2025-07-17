aee75e18cfdba29f3d267d59e769659e2e381c52
Anup kumar <anupk7196@gmail.com>
2025-07-17 12:45:29 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,24 @@ import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 const Navbar = () => {
 
     const {openSignIn} = useClerk();
+    const {user} = useUser();
 
   return (
     <div className='shadow py-4'>
       <div className='container px-2 2xl:px-20 mx-auto flex justify-between items-center'>
         <img src={assets.logo1} atl="Logo" style={{ width: "150px", height: "auto" }}/>
-        <div className='flex gap-4 max-sm:text-xs'>
+        {
+            user ? <div>
+                <Link to={'/applications'}>Applied Jobs</Link>
+                <p></p>
+                <p>Hii, {user.firstName+ " "+user.lastName}</p>
+                <UserButton/>
+            </div> : <div className='flex gap-4 max-sm:text-xs'>
             <button className='text-gray-600 cursor-pointer'>Recruiter Login</button>
             <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full cursor-pointer'>Login</button>
         </div>
+        }
+        
       </div>
     </div>
     
