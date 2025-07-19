bcf978c64696e1b03b2359d6bb2fcef7c1e62a4b
Anup kumar <anupk7196@gmail.com>
2025-07-19 17:39:37 +0530

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <img src={assets.logo1} alt="" />
+      <p>Copyright @Code2Career.dev | All right reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
