d11fb51bb302a2222e732ab4ad40e61fdf45886a
Anup kumar <anupk7196@gmail.com>
2025-07-19 17:27:30 +0530

    appdownload

---
+++ src/components/AppDownload.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { assets } from '../assets/assets';
+
+const AppDownload = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1>Download Mobile App for Better Experience</h1>
+            <div>
+                <a href="">
+                    <img src={assets.play_store} alt="" />
+                </a>
+                <a href="">
+                    <img src={assets.app_store} alt="" />
+                </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AppDownload;
